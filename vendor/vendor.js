fa9f3844d8c6b37ea4e01a58378fa9f9043b0d1b
plangdon <plangdon317@gmail.com>
2017-05-10 15:26:26 -0400

    Colors

--- vendor/vendor.js
+++ vendor/vendor.js
@@ -9,6 +9,7 @@
 
 var vendor = {
 	'moment.js' : 'moment/moment-with-locales.js',
+	'ntc.js' : 'ntc/ntc.js',
 	'weather-icons.css': 'weather-icons/css/weather-icons.css',
 	'weather-icons-wind.css': 'weather-icons/css/weather-icons-wind.css',
 	'font-awesome.css': 'font-awesome-4.7.0/css/font-awesome.min.css'
